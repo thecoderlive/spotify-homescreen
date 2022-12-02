5f3a101e6f126a0b0d04430b11890d57f2b604c3
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-02 19:47:28 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,181 @@
+export const item = {
+    "id": "36ac9be3-257a-403a-93a9-92bb3aa176ec",
+    "trending_event": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png",
+    "home_feed": [
+        {
+            "id": "56da27a7-e2dd-418c-b2a1-15242ce8e3fe",
+            "home_carousel": [
+                {
+                    "id": "c6c2419a-8400-4f35-bb1d-812eefb43f29",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png",
+                    "song_singer": "window",
+                    "song_description": "try"
+                },
+                {
+                    "id": "4e2caf59-25eb-4cd4-b941-e8aa2369128b",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png",
+                    "song_singer": "seven",
+                    "song_description": "life"
+                },
+                {
+                    "id": "1837ec1b-c0e6-478a-8bc5-3e90d4847431",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png",
+                    "song_singer": "PM",
+                    "song_description": "serve"
+                },
+                {
+                    "id": "f324fc74-da16-411f-8f17-4085ce496aa9",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png",
+                    "song_singer": "moment",
+                    "song_description": "beautiful"
+                },
+                {
+                    "id": "a30edc58-b9f9-4b4f-a879-224d24371f10",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png",
+                    "song_singer": "act",
+                    "song_description": "open"
+                }
+            ]
+        },
+        {
+            "id": "b9ea6aaa-e35e-4ac8-b2a1-e27b168b6788",
+            "home_carousel": [
+                {
+                    "id": "6557b51d-9332-441b-8c47-01885ad7806d",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/310.png",
+                    "song_singer": "see",
+                    "song_description": "letter"
+                },
+                {
+                    "id": "4cd4a359-c00f-4765-ba76-9633e6b7aa6a",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png",
+                    "song_singer": "accept",
+                    "song_description": "energy"
+                },
+                {
+                    "id": "6014b74d-7b50-4cf9-89b3-1da2b6fbe1da",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/266.png",
+                    "song_singer": "as",
+                    "song_description": "indeed"
+                },
+                {
+                    "id": "2f268fe7-8a86-45fb-a94a-9020a3a3b3e2",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png",
+                    "song_singer": "down",
+                    "song_description": "can"
+                },
+                {
+                    "id": "7c96113b-d403-4c33-80bf-2bdb9c38bbb3",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/63.png",
+                    "song_singer": "table",
+                    "song_description": "tonight"
+                }
+            ]
+        },
+        {
+            "id": "3c6a1b91-8515-4c28-9018-279678b6b919",
+            "home_carousel": [
+                {
+                    "id": "0438b05c-bcfb-41f9-b30d-356e8f9a6dc7",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png",
+                    "song_singer": "event",
+                    "song_description": "beautiful"
+                },
+                {
+                    "id": "47ea7a4a-f921-4101-ad36-5c87d7c4a52c",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png",
+                    "song_singer": "industry",
+                    "song_description": "important"
+                },
+                {
+                    "id": "7cbdde00-0e4d-4a67-917b-678b7fa89362",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png",
+                    "song_singer": "as",
+                    "song_description": "home"
+                },
+                {
+                    "id": "fe0a0087-66ad-438e-a8cc-c778ab688a97",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png",
+                    "song_singer": "life",
+                    "song_description": "hear"
+                },
+                {
+                    "id": "d44f7832-8b4b-4302-b556-2c253db830a4",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png",
+                    "song_singer": "black",
+                    "song_description": "bag"
+                }
+            ]
+        },
+        {
+            "id": "1ced7d10-2c70-49e4-ad3c-21647501e04e",
+            "home_carousel": [
+                {
+                    "id": "ac4fa5a7-8a21-4f2f-9861-24f8f0c1697e",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png",
+                    "song_singer": "mean",
+                    "song_description": "especially"
+                },
+                {
+                    "id": "67c0727d-2e27-4143-9755-488585d0c765",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png",
+                    "song_singer": "main",
+                    "song_description": "care"
+                },
+                {
+                    "id": "f4e1df83-e11f-4aec-a938-371d412d2364",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/363.png",
+                    "song_singer": "parent",
+                    "song_description": "player"
+                },
+                {
+                    "id": "65587a2c-3163-4c50-84d2-e25b572be47f",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png",
+                    "song_singer": "employee",
+                    "song_description": "down"
+                },
+                {
+                    "id": "7df7ea76-5eda-430b-beca-0c2863b30af0",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png",
+                    "song_singer": "sit",
+                    "song_description": "where"
+                }
+            ]
+        },
+        {
+            "id": "3ff9d6b6-4df7-47ed-adae-73c3de093143",
+            "home_carousel": [
+                {
+                    "id": "afb1013f-1c2b-430d-9028-23dbb0e0da99",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png",
+                    "song_singer": "clearly",
+                    "song_description": "world"
+                },
+                {
+                    "id": "acb0d138-ccef-4703-b137-ddbf5b4e8b2b",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png",
+                    "song_singer": "man",
+                    "song_description": "simply"
+                },
+                {
+                    "id": "b7774edf-bb06-4817-9446-a7a8b03909b5",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png",
+                    "song_singer": "clearly",
+                    "song_description": "garden"
+                },
+                {
+                    "id": "59ad7638-8175-47e1-8643-49edc0361f4c",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png",
+                    "song_singer": "fast",
+                    "song_description": "want"
+                },
+                {
+                    "id": "bad9c70a-1f96-493a-b0f1-90ae57ad047a",
+                    "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png",
+                    "song_singer": "situation",
+                    "song_description": "dog"
+                }
+            ]
+        }
+    ]
+}
